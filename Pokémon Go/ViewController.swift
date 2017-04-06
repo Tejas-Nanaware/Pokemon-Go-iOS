dac263842949bde5f6ddd919942e416cc02ae7ba
admin <admin@ACE20.local>
2017-04-06 11:30:34 +0530

    Added map with current location

--- Pokémon Go/ViewController.swift
+++ Pokémon Go/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    var manager = CLLocationManager()
+    var update = 0 // Counts till 4 then stops updating location
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        self.manager.delegate = self
 
+        // Check and request authorization status and location showing
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            self.mapView.showsUserLocation = true
+            self.manager.startUpdatingLocation()
+        }
+        else {
+            self.manager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    // To set map region
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if update < 4 {
+            let region = MKCoordinateRegionMakeWithDistance(self.manager.location!.coordinate, 400, 400)
+            self.mapView.setRegion(region, animated: true)
+            update += 1
+        }
+        else {
+            self.manager.stopUpdatingLocation()
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
